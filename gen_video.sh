04aa164d3b8a9b899fc273d3a0addcb552c13dc0
EC2 Default User <ec2-user@ip-172-16-65-0.ec2.internal>
2022-06-30 09:53:26 +0000

    add genVideo.sh

---
+++ gen_video.sh
@@ -0,0 +1,1 @@
+python gen_video.py --output=out/lerp.mp4 --trunc=1 --seeds=0-31 --grid=4x2 --network=https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-r-afhqv2-512x512.pkl
